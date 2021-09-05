a0cd12b4bb5b3317ffa6d5cdd5ad0db0fe7f9a5d
namhihi <yentth237@gmail.com>
2021-09-05 09:37:25 +0700

    add some config app

--- src/server.js
+++ src/server.js
@@ -1,20 +1,35 @@
-import {envVariables, dbConnection} from "./configs";
+import { envVariables, dbConnection } from "./configs";
 
 import express from "express";
 const morgan = require("morgan");
-
-const {PORT, MONGO_URL} = envVariables;
+import path from "path";
+const { PORT, MONGO_URL } = envVariables;
 
 const app = express();
 
 dbConnection(MONGO_URL);
 
 app.use(morgan("combined"));
+app.use(express.static('./src/public'));
+app.use(express.static(path.join(__dirname, '/')));
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true,
+	}),
+);
 
+// config template engine
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+// home route
 app.get("/", (req, res) => {
-    res.send("Hello World");
+	res.render('home/index.ejs');
 });
 
+
+// start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+	console.log(`Server is running on port ${PORT}`);
+});
